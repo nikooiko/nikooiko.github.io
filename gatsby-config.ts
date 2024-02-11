7875c7850236efbbda5374286933eb12165dbbff
Nikos Oikonomou <nikos.oikonomou@kariera.gr>
2024-02-11 17:50:44 +0200

    gh-pages deploy setup

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -2,21 +2,30 @@ import type { GatsbyConfig } from "gatsby";
 
 const config: GatsbyConfig = {
   siteMetadata: {
-    title: `Nikos Oikonomou`,
-    siteUrl: `https://www.yourdomain.tld`
+    title: `About | Nikos Oikonomou`,
+    description: "Nikos Oikonomou Personal Site.",
+    siteUrl: `https://nikooiko.github.io`,
+    author: "Nikos Oikonomou",
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: ["gatsby-plugin-postcss", "gatsby-plugin-image", "gatsby-plugin-sitemap", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
+  plugins: [
+    "gatsby-plugin-postcss",
+    "gatsby-plugin-image",
+    "gatsby-plugin-sitemap",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
     },
-    __key: "images"
-  }]
+  ],
 };
 
 export default config;
